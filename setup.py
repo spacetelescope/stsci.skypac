29e0790bce9e3227257f83ccf2242e95f60fb4f3
Joseph Hunkeler <jhunkeler@gmail.com>
2018-05-18 11:43:54 -0400

    * Update RELIC logic * Remove whitespace around kwargs

--- setup.py
+++ setup.py
@@ -1,40 +1,40 @@
 #!/usr/bin/env python
 import os
-import subprocess
+import pkgutil
 import sys
-from glob import glob
-from numpy import get_include as np_include
-from setuptools import setup, find_packages, Extension
+from setuptools import setup, find_packages
+from subprocess import check_call, CalledProcessError
 
 
-if os.path.exists('relic'):
-    sys.path.insert(1, 'relic')
-    import relic.release
-else:
+if not pkgutil.find_loader('relic'):
+    relic_local = os.path.exists('relic')
+    relic_submodule = (relic_local and
+                       os.path.exists('.gitmodules') and
+                       not os.listdir('relic'))
     try:
-        import relic.release
-    except ImportError:
-        try:
-            subprocess.check_call(['git', 'clone',
-                'https://github.com/jhunkeler/relic.git'])
-            sys.path.insert(1, 'relic')
-            import relic.release
-        except subprocess.CalledProcessError as e:
-            print(e)
-            exit(1)
+        if relic_submodule:
+            check_call(['git', 'submodule', 'update', '--init', '--recursive'])
+        elif not relic_local:
+            check_call(['git', 'clone', 'https://github.com/spacetelescope/relic.git'])
 
+        sys.path.insert(1, 'relic')
+    except CalledProcessError as e:
+        print(e)
+        exit(1)
+
+import relic.release
 
 version = relic.release.get_info()
 relic.release.write_template(version, 'stsci/skypac')
 
 setup(
-    name = 'stsci.skypac',
-    version = version.pep386,
-    author = 'Mihai Cara, Warren Hack, Pey Lian Lim',
-    author_email = 'help@stsci.edu',
-    description = 'Sky matching on image mosaic',
-    url = 'https://github.com/spacetelescope/stsci.skypac',
-    classifiers = [
+    name='stsci.skypac',
+    version=version.pep386,
+    author='Mihai Cara, Warren Hack, Pey Lian Lim',
+    author_email='help@stsci.edu',
+    description='Sky matching on image mosaic',
+    url='https://github.com/spacetelescope/stsci.skypac',
+    classifiers=[
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
@@ -42,7 +42,7 @@ setup(
         'Topic :: Scientific/Engineering :: Astronomy',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
-    install_requires = [
+    install_requires=[
         'astropy',
         'nose',
         'numpy',
@@ -52,8 +52,8 @@ setup(
         'stsci.tools',
         'stwcs'
     ],
-    packages = find_packages(),
-    package_data = {
+    packages=find_packages(),
+    package_data={
         '': ['LICENSE.txt'],
         'stsci/skypac': [
             '*.help',
